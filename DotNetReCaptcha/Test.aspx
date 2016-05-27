2103e65d224af6e9363026d6f371f02e31c080f8
Halit Yurttas <halityurttas@gmail.com>
2016-05-27 15:40:51 +0300

    Test.aspx eklendi

---
+++ DotNetReCaptcha/Test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="DotNetReCaptcha.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="g-recaptcha" data-sitekey="6Lc8lwcTAAAAAC-Prj1Py8epgTnWbJhLJxnP3pkI"></div>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <asp:Button ID="btnsend" runat="server" Text="Gönder" OnClick="btnsend_Click" />
+    <asp:Label ID="lblResult" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ DotNetReCaptcha/Test.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNetReCaptcha
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsend_Click(object sender, EventArgs e)
+        {
+            Helpers.CaptchaHelper captha = new Helpers.CaptchaHelper(HttpContext.Current);
+            if (captha.Validate())
+            {
+                lblResult.Text = "Tamamdır";
+            }
+            else
+            {
+                lblResult.Text = "Olmadı";
+            }
+        }
+    }
+}
